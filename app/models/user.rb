ed5c4e6119fb25227251369a235185ff8cdce42b
Ronnie <cornatov@buffalo.edu>
2017-07-16 23:52:57 +0000

    fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :orders
-  has_many :comments
-end
+    has_many :orders
+    
+  after_create :send_welcome_email
+  def send_welcome_email
+    UserMailer.send_new_user_message(self).deliver
+  end
+    
 
+end
