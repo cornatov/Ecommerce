b2b25eede539d01e6d173ad01d7b7e88d4720e4e
Ronnie <cornatov@buffalo.edu>
2017-08-31 02:03:34 +0000

    bugs 8/30/17

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,9 @@
+#registrations_controller.rb
+class RegistrationsController < Devise::RegistrationsController
+
+  def create
+    super
+    UserMailer.welcome(resource).deliver unless resource.invalid?
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
 
-devise_for :users do
- get "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
-end
   
-  resources :users
+ devise_for :users 
 
   resources :payments, only: [:new, :create]
+  
+  resources :users
 
   resources :products do
     resources :comments
